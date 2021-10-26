d8c31e99e5056ee109070ac6ffdc278c26a5f3aa
Ryan Long <ryanlong1004@gmail.com>
2021-10-26 14:56:32 -0400

    Point to /home/esmf-profiler/tracees

--- go.sh
+++ go.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-esmf-profiler -t /home/traces -n 'testa' -o /home/traces && pwd && ls -lah /home/traces
+esmf-profiler -t /home/esmf-profiler/traces -n 'testa' -o /home/esmf-profiler/traces && pwd && ls -lah /home/traces
