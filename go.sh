0a0f20c5c7b7386642a43ade89e7accf2d6f44b3
Ryan Long <ryanlong1004@gmail.com>
2021-10-26 14:19:25 -0400

    Add debugging

--- go.sh
+++ go.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-esmf-profiler -t /home/traces -n 'testa' -o /home/traces && ls -lah /home/traces
+esmf-profiler -t /home/traces -n 'testa' -o /home/traces && pwd && ls -lah /home/traces
