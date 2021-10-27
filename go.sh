05ac67b22a5e3bde1a151978d75ed80eab169930
Ryan Long <ryanlong1004@gmail.com>
2021-10-27 19:11:23 -0400

    Add debugging

--- go.sh
+++ go.sh
@@ -2,3 +2,6 @@
 
 cd /home/esmf-profiler/traces
 esmf-profiler -t . -n 'testa' -o . && pwd && ls -lah .
+ls -lah /home/runner/work/esmf-profiler/esmf-profiler
+ls -lah /home/runner/work/esmf-profiler/esmf-profiler/traces
+ls -lah /home/runner/work/esmf-profiler/esmf-profiler/data
